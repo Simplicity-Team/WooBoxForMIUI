6c61a91871e859dfccf4b70f49bf7926c0d7e79a
LittleTurtle2333 <841474544@qq.com>
2022-02-18 17:11:08 +0800

    新增 总开关

--- app/src/main/java/com/lt2333/simplicitytools/hook/MainHook.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/MainHook.kt
@@ -1,29 +1,38 @@
 package com.lt2333.simplicitytools.hook
 
-import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+import com.lt2333.simplicitytools.BuildConfig
+import com.lt2333.simplicitytools.hook.app.Android
+import com.lt2333.simplicitytools.hook.app.MiuiHome
 import com.lt2333.simplicitytools.hook.app.PowerKeeper
 import com.lt2333.simplicitytools.hook.app.SystemUI
-import com.lt2333.simplicitytools.hook.app.MiuiHome
-import com.lt2333.simplicitytools.hook.app.Android
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 class MainHook : IXposedHookLoadPackage {
+
+    var prefs = XSharedPreferences(BuildConfig.APPLICATION_ID, "config")
+
     override fun handleLoadPackage(lpparam: LoadPackageParam) {
 
-        when (lpparam.packageName) {
-            "com.miui.powerkeeper" -> {
-                PowerKeeper().handleLoadPackage(lpparam)
+        if (prefs.getBoolean("main_switch", true)) {
+
+            when (lpparam.packageName) {
+                "com.miui.powerkeeper" -> {
+                    PowerKeeper().handleLoadPackage(lpparam)
+                }
+                "com.android.systemui" -> {
+                    SystemUI().handleLoadPackage(lpparam)
+                }
+                "com.miui.home" -> {
+                    MiuiHome().handleLoadPackage(lpparam)
+                }
+                "android" -> {
+                    Android().handleLoadPackage(lpparam)
+                }
             }
-            "com.android.systemui" -> {
-                SystemUI().handleLoadPackage(lpparam)
-            }
-            "com.miui.home" -> {
-                MiuiHome().handleLoadPackage(lpparam)
-            }
-            "android" -> {
-                Android().handleLoadPackage(lpparam)
-            }
+
         }
-        
+
     }
 }
