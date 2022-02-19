ed9dda02b322c12be1f48f0877643e28f01ecbb1
LittleTurtle2333 <841474544@qq.com>
2022-02-20 01:28:05 +0800

    更新build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.majorVersion
+        jvmTarget = "1.8"
     }
     packagingOptions {
         resources {
