e8b7239fefa0e0dfcee5cf2d215de7804c7256d3
乌堆小透明 <841474544@qq.com>
2022-02-25 21:49:28 +0800

    更新版本号至1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lt2333.simplicitytools"
         minSdk = 31
         targetSdk = 32
-        versionCode = 20
-        versionName = "1.1.9"
+        versionCode = 21
+        versionName = "1.2.0"
     }
 
     buildTypes {
