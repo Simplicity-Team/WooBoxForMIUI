10a6aa400313bcb81e03839aa0cb83a7d55398d2
乌堆小透明 <lturtle2333@outlook.com>
2022-03-09 23:18:36 +0800

    新增 去除自动连招开关开关

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/SecurityCenter.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/SecurityCenter.kt
@@ -12,10 +12,9 @@ class SecurityCenter : IXposedHookLoadPackage {
         XposedBridge.log("成功Hook: " + javaClass.simpleName)
         //跳过 5/10秒等待时间
         SkipWaitingTime().handleLoadPackage(lpparam)
+        //锁定 100分
         LockOneHundred().handleLoadPackage(lpparam)
-
         //去除自动连招黑名单
         RemoveMacroBlacklist().handleLoadPackage(lpparam)
-
     }
 }
--- app/src/main/java/com/lt2333/simplicitytools/hook/app/securitycenter/RemoveMacroBlacklist.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/securitycenter/RemoveMacroBlacklist.kt
@@ -1,5 +1,6 @@
 package com.lt2333.simplicitytools.hook.app.securitycenter
 
+import com.lt2333.simplicitytools.util.XSPUtils
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
@@ -7,21 +8,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class RemoveMacroBlacklist : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        var letter = 'a'
-        for (i in 0..25) {
-            val classIfExists = XposedHelpers.findClassIfExists(
-                "com.miui.gamebooster.v.$letter" + "0", lpparam.classLoader
-            ) ?: continue
-            if (classIfExists.declaredMethods.size in 6..12 && classIfExists.fields.isEmpty() && classIfExists.declaredFields.size >= 2) {
-                XposedHelpers.findAndHookMethod(classIfExists, "c", String::class.java,
-                    object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam?) {
-                            param?.result = false
-                        }
-                    })
-                return
+        if (XSPUtils.getBoolean("remove_macro_blacklist", false)) {
+            var letter = 'a'
+            for (i in 0..25) {
+                val classIfExists = XposedHelpers.findClassIfExists(
+                    "com.miui.gamebooster.v.$letter" + "0", lpparam.classLoader
+                ) ?: continue
+                if (classIfExists.declaredMethods.size in 6..12 && classIfExists.fields.isEmpty() && classIfExists.declaredFields.size >= 2) {
+                    XposedHelpers.findAndHookMethod(classIfExists, "c", String::class.java,
+                        object : XC_MethodHook() {
+                            override fun beforeHookedMethod(param: MethodHookParam?) {
+                                param?.result = false
+                            }
+                        })
+                    return
+                }
+                letter++
             }
-            letter++
         }
     }
 }
