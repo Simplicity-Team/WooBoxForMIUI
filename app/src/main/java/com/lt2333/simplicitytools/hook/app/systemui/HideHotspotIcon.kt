6d8f78a99702b042fa2f3e0581c2a8b0997965f9
QQ littleice <1767523953@qq.com>
2022-03-10 19:10:04 +0800

    simplify HideHotspotIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideHotspotIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideHotspotIcon.kt
@@ -1,27 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideHotspotIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.PhoneStatusBarPolicy\$2",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "onHotspotChanged",
-            Boolean::class.java, Int::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_hotspot_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.PhoneStatusBarPolicy\$2".hookBeforeMethod(lpparam.classLoader, "onHotspotChanged", Boolean::class.java, Int::class.java) {
+            hasEnable("hide_hotspot_icon") {
+                it.result = null
+            }
+        }
     }
 }
