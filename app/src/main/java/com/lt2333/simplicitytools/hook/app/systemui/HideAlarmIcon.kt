cd14f89364e8cd98e463d82d56e64665927a42e6
QQ littleice <1767523953@qq.com>
2022-03-10 18:41:35 +0800

    simplify HideAlarmIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideAlarmIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideAlarmIcon.kt
@@ -1,26 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideAlarmIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "onMiuiAlarmChanged",
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_alarm_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy".hookBeforeMethod(lpparam.classLoader, "onMiuiAlarmChanged") {
+            hasEnable("hide_alarm_icon") {
+                it.result = null
+            }
+        }
     }
 }
