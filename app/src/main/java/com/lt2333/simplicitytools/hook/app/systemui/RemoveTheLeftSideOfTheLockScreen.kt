76156cbf684d4b49d46dba17bef8add45aa96d7e
QQ littleice <1767523953@qq.com>
2022-03-10 22:15:30 +0800

    simplify RemoveTheLeftSideOfTheLockScreen.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/RemoveTheLeftSideOfTheLockScreen.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/RemoveTheLeftSideOfTheLockScreen.kt
@@ -1,26 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class RemoveTheLeftSideOfTheLockScreen : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.keyguard.negative.MiuiKeyguardMoveLeftViewContainer",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "inflateLeftView",
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("remove_the_left_side_of_the_lock_screen", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.keyguard.negative.MiuiKeyguardMoveLeftViewContainer".hookBeforeMethod(lpparam.classLoader, "inflateLeftView") {
+            hasEnable("remove_the_left_side_of_the_lock_screen") {
+                it.result = null
+            }
+        }
     }
 }
