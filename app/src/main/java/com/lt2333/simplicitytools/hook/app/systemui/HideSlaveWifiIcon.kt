71e77733193b189aa6467fed799a42e3429042b8
QQ littleice <1767523953@qq.com>
2022-03-10 21:57:45 +0800

    simplify HideSlaveWifiIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideSlaveWifiIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideSlaveWifiIcon.kt
@@ -1,26 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideSlaveWifiIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.policy.SlaveWifiSignalController",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "updateIconState",
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_slave_wifi_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.policy.SlaveWifiSignalController".hookBeforeMethod(lpparam.classLoader, "updateIconState") {
+            hasEnable("hide_slave_wifi_icon") {
+                it.result = null
+            }
+        }
     }
 }
