793c04916f97d2fc0e10e1a779dcaa2377e88a1c
QQ littleice <1767523953@qq.com>
2022-03-10 18:54:38 +0800

    simplify HideBluetoothHandsfreeBatteryIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideBluetoothHandsfreeBatteryIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideBluetoothHandsfreeBatteryIcon.kt
@@ -1,28 +1,17 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
 import android.content.Intent
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideBluetoothHandsfreeBatteryIcon : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "updateBluetoothHandsfreeBattery",
-            Intent::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_bluetooth_battery_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy".hookBeforeMethod(lpparam.classLoader, "updateBluetoothHandsfreeBattery", Intent::class.java) {
+            hasEnable("hide_bluetooth_battery_icon") {
+                it.result = null
+            }
+        }
     }
 }
