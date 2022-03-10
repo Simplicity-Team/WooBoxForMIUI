3becebbdce006e69e54313c3083f925bebd6bd28
QQ littleice <1767523953@qq.com>
2022-03-10 18:56:14 +0800

    simplify HideBluetoothIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideBluetoothIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideBluetoothIcon.kt
@@ -1,27 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideBluetoothIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "updateBluetooth",
-            String::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_bluetooth_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy".hookBeforeMethod(lpparam.classLoader, "updateBluetooth", String::class.java) {
+            hasEnable("hide_bluetooth_icon") {
+                it.result = null
+            }
+        }
     }
 }
