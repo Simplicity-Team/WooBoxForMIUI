c766de062e51fb6fff9c17dc67b7d476b64c3216
QQ littleice <1767523953@qq.com>
2022-03-10 21:59:01 +0800

    simplify HideVolumeZenIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideVolumeZenIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideVolumeZenIcon.kt
@@ -1,26 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideVolumeZenIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "updateVolumeZen",
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_volume_zen_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy".hookBeforeMethod(lpparam.classLoader, "updateVolumeZen") {
+            hasEnable("hide_volume_zen_icon") {
+                it.result = null
+            }
+        }
     }
 }
