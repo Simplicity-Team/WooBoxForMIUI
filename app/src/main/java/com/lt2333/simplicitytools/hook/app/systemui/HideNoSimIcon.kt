032d6e54854c24066c9f2a9155dc03513e707b47
QQ littleice <1767523953@qq.com>
2022-03-10 19:12:32 +0800

    simplify HideNoSimIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideNoSimIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideNoSimIcon.kt
@@ -1,28 +1,16 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideNoSimIcon : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiStatusBarSignalPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "setNoSims",
-            Boolean::class.javaPrimitiveType,
-            Boolean::class.javaPrimitiveType,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_no_sim_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiStatusBarSignalPolicy".hookBeforeMethod(lpparam.classLoader, "setNoSims", Boolean::class.javaPrimitiveType, Boolean::class.javaPrimitiveType) {
+            hasEnable("hide_no_sim_icon") {
+                it.result = null
+            }
+        }
     }
 }
