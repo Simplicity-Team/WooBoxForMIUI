6ba5a349964e88865c8a4cd0c59ac930e19d28fd
QQ littleice <1767523953@qq.com>
2022-03-10 19:08:10 +0800

    simplify HideHeadsetIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideHeadsetIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideHeadsetIcon.kt
@@ -1,28 +1,17 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
 import android.content.Intent
-import com.lt2333.simplicitytools.util.XSPUtils
+import com.lt2333.simplicitytools.util.hasEnable
+import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideHeadsetIcon :IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val classIfExists = XposedHelpers.findClassIfExists(
-            "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy",
-            lpparam.classLoader
-        )
-        XposedHelpers.findAndHookMethod(
-            classIfExists,
-            "updateHeadsetPlug",
-            Intent::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    if (XSPUtils.getBoolean("hide_headset_icon", false)) {
-                        param.result = null
-                    }
-                }
-            })
+        "com.android.systemui.statusbar.phone.MiuiPhoneStatusBarPolicy".hookBeforeMethod(lpparam.classLoader, "updateHeadsetPlug", Intent::class.java) {
+            hasEnable("hide_headset_icon") {
+                it.result = null
+            }
+        }
     }
 }
