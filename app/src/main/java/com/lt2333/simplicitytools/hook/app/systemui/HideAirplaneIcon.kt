97c8307447d3f61ece56ffe0bcaeb40b0eb12e53
QQ littleice <1767523953@qq.com>
2022-03-10 19:02:01 +0800

    Update HideAirplaneIcon.kt

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideAirplaneIcon.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/systemui/HideAirplaneIcon.kt
@@ -1,6 +1,5 @@
 package com.lt2333.simplicitytools.hook.app.systemui
 
-import com.lt2333.simplicitytools.util.findClass
 import com.lt2333.simplicitytools.util.hasEnable
 import com.lt2333.simplicitytools.util.hookBeforeMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
@@ -8,7 +7,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HideAirplaneIcon : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        val iconState = "com.android.systemui.statusbar.policy.NetworkController\$IconState".findClass(lpparam.classLoader)
+        val iconState = "com.android.systemui.statusbar.policy.NetworkController\$IconState"
         "com.android.systemui.statusbar.phone.StatusBarSignalPolicy".hookBeforeMethod(lpparam.classLoader, "setIsAirplaneMode", iconState) {
             hasEnable("hide_airplane_icon") {
                 it.result = null
