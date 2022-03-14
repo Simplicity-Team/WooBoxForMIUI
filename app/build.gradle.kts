11908a7381009d0b183a4a12ae7bd93ce0ea38e1
乌堆小透明 <lturtle2333@outlook.com>
2022-03-15 01:07:12 +0800

    Update version to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lt2333.simplicitytools"
         minSdk = 31
         targetSdk = 32
-        versionCode = 44
-        versionName = "1.4.3"
+        versionCode = 45
+        versionName = "1.4.4"
     }
 
     buildTypes {
