61f64ba7bbffba8c22d77d33becfd07812edd572
LittleTurtle2333 <lturtle2333@outlook.com>
2022-03-24 16:45:11 +0800

    修复开机时的一处错误

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/android/MaxWallpaperScale.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/android/MaxWallpaperScale.kt
@@ -1,9 +1,6 @@
 package com.lt2333.simplicitytools.hook.app.android
 
-import android.app.Service
-import android.icu.text.DisplayContext
 import com.lt2333.simplicitytools.util.XSPUtils
-import com.lt2333.simplicitytools.util.hookAfterMethod
 import com.lt2333.simplicitytools.util.hookBeforeMethod
 import com.lt2333.simplicitytools.util.setFloatField
 import de.robv.android.xposed.IXposedHookLoadPackage
@@ -11,11 +8,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class MaxWallpaperScale : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        "com.android.server.wm.WallpaperController".hookAfterMethod(lpparam.classLoader, "WallpaperController", Service::class.java, DisplayContext::class.java
-        ) {
-            val value = XSPUtils.getFloat("max_wallpaper_scale", 1.1f)
-            it.thisObject.setFloatField("mMaxWallpaperScale", value)
-        }
+
         "com.android.server.wm.WallpaperController".hookBeforeMethod(lpparam.classLoader, "zoomOutToScale", Float::class.java) {
             val value = XSPUtils.getFloat("max_wallpaper_scale", 1.1f)
             it.thisObject.setFloatField("mMaxWallpaperScale", value)
