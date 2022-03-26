6f91cab9b812ba954d43009ef7557aa99813806d
LittleTurtle2333 <lturtle2333@outlook.com>
2022-03-26 16:31:52 +0800

    Update version to 1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lt2333.simplicitytools"
         minSdk = 31
         targetSdk = 32
-        versionCode = 54
-        versionName = "1.5.3"
+        versionCode = 55
+        versionName = "1.5.4"
     }
 
     buildTypes {
