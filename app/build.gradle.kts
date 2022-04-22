7e922f3ec6275091b1cb9e1190e36f8d86bac084
LittleTurtle2333 <lturtle2333@outlook.com>
2022-04-23 01:29:21 +0800

    更新依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,16 +51,14 @@ android {
 }
 
 dependencies {
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     //API
     compileOnly("de.robv.android.xposed:api:82")
+    implementation("com.github.kyuubiran:EzXHelper:0.8.6")
     //UI
     implementation(project(":blockmiui"))
-
-    val appCenterSdkVersion = "4.3.1"
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    //APP Center
+    val appCenterSdkVersion = "4.4.3"
     implementation("com.microsoft.appcenter:appcenter-analytics:${appCenterSdkVersion}")
     implementation("com.microsoft.appcenter:appcenter-crashes:${appCenterSdkVersion}")
-
-    implementation("com.github.kyuubiran:EzXHelper:0.7.4")
-
 }
