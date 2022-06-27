4246e2910ec74548776bdc2ef187f74de438cae4
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-06-28 00:51:43 +0800

    修复 `截图无限裁切` 功能导致的崩溃问题 (#172)
    * 同时更新依赖
    * 允许应用横屏

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
