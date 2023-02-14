b781f594868d2362f5f3dd1eb2a9c1a28d0cae48
LittleTurtle2333 <lturtle2333@outlook.com>
2023-02-14 19:38:03 +0800

    修复A13自定义网络类型文本不可用

--- app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/systemui/CustomMobileTypeTextForT.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/systemui/CustomMobileTypeTextForT.kt
@@ -9,8 +9,7 @@ import com.lt2333.simplicitytools.utils.xposed.base.HookRegister
 object CustomMobileTypeTextForT : HookRegister() {
 
     override fun init() = hasEnable("custom_mobile_type_text_switch") {
-        // TODO: Android13自定义网络类型文本不可用
-        findMethod("com.android.systemui.statusbar.policy.MobileSignalController") {
+        findMethod("com.android.systemui.statusbar.connectivity.MobileSignalController") {
             name == "getMobileTypeName" && parameterTypes[0] == Int::class.java
         }.hookAfter {
             it.result = XSPUtils.getString("custom_mobile_type_text", "5G")
