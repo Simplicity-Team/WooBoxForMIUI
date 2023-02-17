f2a4f0630155670fc123d4b0c74d43ad8cd041ea
LittleTurtle2333 <lturtle2333@outlook.com>
2023-02-17 17:31:49 +0800

    修复连接WiFi时非上网卡显示大移动类型

--- app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/systemui/StatusBarBigMobileTypeIconForT.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/systemui/StatusBarBigMobileTypeIconForT.kt
@@ -16,12 +16,10 @@ object StatusBarBigMobileTypeIconForT : HookRegister() {
 
     private val getLocation = XSPUtils.getInt("big_mobile_type_location", 1)
     private val upAndDownPosition = XSPUtils.getInt("big_mobile_type_icon_up_and_down_position", 0)
-    private val leftAndRightMargin =
-        XSPUtils.getInt("big_mobile_type_icon_left_and_right_margins", 0)
+    private val leftAndRightMargin = XSPUtils.getInt("big_mobile_type_icon_left_and_right_margins", 0)
     private val isBold = XSPUtils.getBoolean("big_mobile_type_icon_bold", true)
     private val size = XSPUtils.getFloat("big_mobile_type_icon_size", 12.5f)
-    private val isOnlyShowNetwork =
-        XSPUtils.getBoolean("big_mobile_type_only_show_network_card", false)
+    private val isOnlyShowNetwork = XSPUtils.getBoolean("big_mobile_type_only_show_network_card", false)
 
     override fun init() = hasEnable("big_mobile_type_icon") {
         //使网络类型单独显示
@@ -41,16 +39,13 @@ object StatusBarBigMobileTypeIconForT : HookRegister() {
             val res: Resources = context.resources
 
             //获取组件
-            val mobileContainerLeftId: Int =
-                res.getIdentifier("mobile_container_left", "id", "com.android.systemui")
-            val mobileContainerLeft =
-                statusBarMobileView.findViewById<ViewGroup>(mobileContainerLeftId)
+            val mobileContainerLeftId: Int = res.getIdentifier("mobile_container_left", "id", "com.android.systemui")
+            val mobileContainerLeft = statusBarMobileView.findViewById<ViewGroup>(mobileContainerLeftId)
 
             val mobileGroupId: Int = res.getIdentifier("mobile_group", "id", "com.android.systemui")
             val mobileGroup = statusBarMobileView.findViewById<ViewGroup>(mobileGroupId)
 
-            val mobileTypeSingleId: Int =
-                res.getIdentifier("mobile_type_single", "id", "com.android.systemui")
+            val mobileTypeSingleId: Int = res.getIdentifier("mobile_type_single", "id", "com.android.systemui")
             val mobileTypeSingle = statusBarMobileView.findViewById<TextView>(mobileTypeSingleId)
 
             //更改顺序
@@ -70,10 +65,8 @@ object StatusBarBigMobileTypeIconForT : HookRegister() {
                 leftAndRightMargin, upAndDownPosition, leftAndRightMargin, 0
             )
             //显示非上网卡的大图标
-            if (!isOnlyShowNetwork) {
-                if (!mobileIconState.getObjectAs<Boolean>("dataConnected")) {
-                    mobileTypeSingle.visibility = View.VISIBLE
-                }
+            if (!mobileIconState.getObjectAs<Boolean>("dataConnected") && !mobileIconState.getObjectAs<Boolean>("wifiAvailable")) {
+                mobileTypeSingle.visibility = View.VISIBLE
             }
         }
         //显示非上网卡的大图标
@@ -86,12 +79,10 @@ object StatusBarBigMobileTypeIconForT : HookRegister() {
                 val context: Context = statusBarMobileView.context
                 val res: Resources = context.resources
 
-                val mobileTypeSingleId: Int =
-                    res.getIdentifier("mobile_type_single", "id", "com.android.systemui")
-                val mobileTypeSingle =
-                    statusBarMobileView.findViewById<TextView>(mobileTypeSingleId)
+                val mobileTypeSingleId: Int = res.getIdentifier("mobile_type_single", "id", "com.android.systemui")
+                val mobileTypeSingle = statusBarMobileView.findViewById<TextView>(mobileTypeSingleId)
 
-                if (!mobileIconState.getObjectAs<Boolean>("dataConnected")) {
+                if (!mobileIconState.getObjectAs<Boolean>("dataConnected") && !mobileIconState.getObjectAs<Boolean>("wifiAvailable")) {
                     mobileTypeSingle.visibility = View.VISIBLE
                 }
             }
