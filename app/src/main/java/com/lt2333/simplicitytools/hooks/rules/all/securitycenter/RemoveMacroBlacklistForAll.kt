7dcfdcced792c77270a82f51327b48519da0e271
LittleTurtle2333 <lturtle2333@outlook.com>
2023-02-17 20:19:39 +0800

    修复去除游戏自动连招黑名单

--- app/src/main/java/com/lt2333/simplicitytools/hooks/rules/all/securitycenter/RemoveMacroBlacklistForAll.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/rules/all/securitycenter/RemoveMacroBlacklistForAll.kt
@@ -1,27 +1,29 @@
 package com.lt2333.simplicitytools.hooks.rules.all.securitycenter
 
+import com.github.kyuubiran.ezxhelper.utils.*
 import com.lt2333.simplicitytools.utils.XSPUtils
 import com.lt2333.simplicitytools.utils.xposed.base.HookRegister
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 
 object RemoveMacroBlacklistForAll : HookRegister() {
 
     override fun init() {
-        // TODO: 新版已失效 
         if (XSPUtils.getBoolean("remove_macro_blacklist", false)) {
             var letter = 'a'
             for (i in 0..25) {
                 val classIfExists = XposedHelpers.findClassIfExists(
-                    "com.miui.gamebooster.v.$letter" + "0", getDefaultClassLoader()
+                    "com.miui.gamebooster.utils.$letter" + "0", getDefaultClassLoader()
                 ) ?: continue
-                if (classIfExists.declaredMethods.size in 6..12 && classIfExists.fields.isEmpty() && classIfExists.declaredFields.size >= 2) {
-                    XposedHelpers.findAndHookMethod(classIfExists, "c", String::class.java,
-                        object : XC_MethodHook() {
-                            override fun beforeHookedMethod(param: MethodHookParam?) {
-                                param?.result = false
-                            }
-                        })
+                if (classIfExists.declaredMethods.size in 10..15 && classIfExists.fields.isEmpty() && classIfExists.declaredFields.size >= 2) {
+                    XposedBridge.log("Woobox: RemoveMacroBlacklist in ${classIfExists.name}")
+                    findMethod(classIfExists){
+                        name=="c" && returnType==Boolean::class.java && isStatic &&parameterCount==1
+                    }.hookBefore {
+                        XposedBridge.log("Woobox: c")
+                        it.result = false
+                    }
                     return
                 }
                 letter++
