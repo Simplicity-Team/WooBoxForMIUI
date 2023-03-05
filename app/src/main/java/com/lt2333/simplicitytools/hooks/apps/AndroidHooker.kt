c81951da2a367c1023601b522637d69cde33551b
LittleTurtle2333 <lturtle2333@outlook.com>
2023-03-05 22:07:28 +0800

    Update `AndroidHooker.kt`

--- app/src/main/java/com/lt2333/simplicitytools/hooks/apps/AndroidHooker.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/apps/AndroidHooker.kt
@@ -8,14 +8,15 @@ import com.lt2333.simplicitytools.hooks.rules.all.android.DisableFlagSecureForAl
 
 object AndroidHooker: YukiBaseHooker() {
     override fun onHook() {
+        //判断Android版本号加载不同的Hooker
         when (Build.VERSION.SDK_INT) {
-            Build.VERSION_CODES.TIRAMISU -> {
+            Build.VERSION_CODES.TIRAMISU -> { //Android13
                 loadHooker(DisableFlagSecureForAll)  //允许截图
                 loadHooker(AllowUntrustedTouchesForAll) //允许不受信任触摸
                 loadHooker(DeleteOnPostNotificationForAll) //上层显示
             }
 
-            Build.VERSION_CODES.S -> {
+            Build.VERSION_CODES.S -> { //Android12
                 loadHooker(DisableFlagSecureForAll)  //允许截图
                 loadHooker(AllowUntrustedTouchesForAll) //允许不受信任触摸
                 loadHooker(DeleteOnPostNotificationForAll) //上层显示
