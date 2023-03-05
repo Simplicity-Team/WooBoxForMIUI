ce5312494cf97d01b1e5a6f8275db32508b84158
LittleTurtle2333 <lturtle2333@outlook.com>
2023-03-05 22:06:42 +0800

    Update `SystemUIHooker.kt`

--- app/src/main/java/com/lt2333/simplicitytools/hooks/apps/SystemUIHooker.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/apps/SystemUIHooker.kt
@@ -2,7 +2,21 @@ package com.lt2333.simplicitytools.hooks.apps
 
 import android.os.Build
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.lt2333.simplicitytools.hooks.rules.t.systemui.*
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideBatteryIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideHDIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideMobileActivityIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideMobileTypeIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideSimIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideStatusBarIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideStatusBarNetworkSpeedSecondForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.HideWifiActivityIconForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.MaximumNumberOfNotificationIconsForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.NewControlCenterWeatherForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.NotificationWeatherForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.RemoveLockScreenCameraForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.RemoveTheLeftSideOfTheLockScreenForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.StatusBarNetworkSpeedRefreshSpeedForT
+import com.lt2333.simplicitytools.hooks.rules.t.systemui.StatusBarTimeCustomizationForT
 
 object SystemUIHooker : YukiBaseHooker() {
     override fun onHook() {
@@ -22,6 +36,7 @@ object SystemUIHooker : YukiBaseHooker() {
                 loadHooker(RemoveTheLeftSideOfTheLockScreenForT) //移除锁屏负一屏功能
                 loadHooker(RemoveLockScreenCameraForT) //移除锁屏相机功能
                 loadHooker(NotificationWeatherForT) //通知面板天气
+                loadHooker(NewControlCenterWeatherForT) //新控制中心天气
             }
 
             Build.VERSION_CODES.S -> {
